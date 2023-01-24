bc4ff4558bb54c45639047f59cc0e643a7734f69
NurdauletSU <123021612+NurdauletSU@users.noreply.github.com>
2023-01-24 11:56:19 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Операция бастапқы ретінде нөл"
+      "\nпрограмма жумыс істеуін тоқтату")
+while True:
+    s = input("Знак (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Нөлге бөлу!")
+    else:
+        print("Операция кате белгысы!")
